8bc3fdd98ef72c2b24f47c4bf4376e8e44f7cf5b
choi ha young <gkdudk15@naver.com>
2023-02-15 14:33:29 +0900

    라우터 페이지 연결확인

---
+++ src/shared/Router.js
@@ -0,0 +1,20 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Contact from '../pages/Contact';
+import Home from '../pages/Home';
+import About from '../pages/About';
+import Works from '../pages/Works';
+
+const Router = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="/works" element={<Works />} />
+            </Routes>
+        </BrowserRouter>
+    );
+};
+
+export default Router;
